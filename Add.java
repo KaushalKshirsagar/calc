a9501e64e84850f300707694352221b3bc461cf9
Kaushal Kshirsagar <83549131+KaushalKshirsagar@users.noreply.github.com>
2025-04-03 15:26:58 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
-public class Add{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first number");
-		int n1 = sc.nextInt();
-		System.out.println("Enter second number");
-		int n2 = sc.nextInt();
+public class Add {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Taking user input
+        System.out.println("Enter first number:");
+        int n1 = sc.nextInt();
+        
+        System.out.println("Enter second number:");
+        int n2 = sc.nextInt();
 
-		int addition = n1 + n2;
-		System.out.println("Addition = ", addition);
+        // Performing addition
+        int addition = n1 + n2;
+        System.out.println("Addition = " + addition);
 
-		sc.close();
-	}
+        // Performing multiplication
+        int multiplication = n1 * n2;
+        System.out.println("Multiplication = " + multiplication);
+        
+        // Closing scanner
+        sc.close();
+    }
 }
+
