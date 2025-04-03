0c309eb027da8a47579fee8b0b2898d2ea424021
kaush <kaush@123.lab>
2025-04-03 15:20:56 +0530

    Add.java created

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Add{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int n1 = sc.nextInt();
+		System.out.println("Enter second number");
+		int n2 = sc.nextInt();
+
+		int addition = n1 + n2;
+		System.out.println("Addition = ", addition);
+
+		sc.close();
+	}
+}
